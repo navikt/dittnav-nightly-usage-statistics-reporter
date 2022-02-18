b117f666cfcab7118cd71aa63bda15b24ddf52d5
Mauricio Burgos Herrera <mauricio.burgos.herrera@nav.no>
2022-02-18 12:00:48 +0100

    Legge til delay for azur mounts, kalle exitProcess etter reportering

--- src/main/kotlin/no/nav/personbruker/dittnav/metrics/main.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/metrics/main.kt
@@ -1,7 +1,18 @@
 package no.nav.personbruker.dittnav.metrics
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import kotlin.system.exitProcess
+
+val log = LoggerFactory.getLogger(Application::class.java)
 
 fun main() = runBlocking {
-    Application().runMetricsReporting()
+    try {
+        log.info("Venter for azure key mount")
+        delay(30 * 1000)
+        Application().runMetricsReporting()
+    } finally {
+        exitProcess(0)
+    }
 }
